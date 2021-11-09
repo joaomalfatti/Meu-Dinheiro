1f021557bb72d5594bf9d53d35f224a4d8bf7fb8
joaomalfatti <joao.cursos1210@gmail.com>
2021-11-08 20:12:31 -0400

    11. Criando os temas de cores da aplicação

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     grid-area: AS;
+    color: ${props => props.theme.colors.white};
+    background-color: ${props => props.theme.colors.secondary};
 
-    background-color: #252A48;
 
 `;
 
